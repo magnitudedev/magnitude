e31933c73998407d10741ece4a293084c1f8d31a
trg <tom@magnitude.dev>
2026-07-25 16:48:42 -0700

    Refactor user message styling

--- cli/src/features/chat-timeline/messages/user-message-frame.tsx
+++ cli/src/features/chat-timeline/messages/user-message-frame.tsx
@@ -7,99 +7,31 @@ interface UserMessageFrameProps {
   readonly children: ReactNode
 }
 
-const edgeChars = (vertical: '╻' | '╹') => ({
-  topLeft: '',
-  bottomLeft: '',
-  topRight: '',
-  bottomRight: '',
-  horizontal: ' ',
-  vertical,
-  topT: '',
-  bottomT: '',
-  leftT: '',
-  rightT: '',
-  cross: '',
-})
-
-const capChars = (horizontal: '▄' | '▀') => ({
-  topLeft: '',
-  bottomLeft: '',
-  topRight: '',
-  bottomRight: '',
-  horizontal,
-  vertical: ' ',
-  topT: '',
-  bottomT: '',
-  leftT: '',
-  rightT: '',
-  cross: '',
-})
-
 export const UserMessageFrame = ({
   borderColor,
   backgroundColor,
   children,
 }: UserMessageFrameProps) => (
-  <>
+  <box
+    style={{
+      borderStyle: 'single',
+      border: ['left'],
+      borderColor,
+      customBorderChars: { ...BOX_CHARS, vertical: '┃' },
+    }}
+  >
     <box
       style={{
-        height: 1,
-        borderStyle: 'single',
-        border: ['left'],
-        borderColor,
-        customBorderChars: edgeChars('╻'),
+        flexDirection: 'row',
+        backgroundColor,
+        paddingTop: 1,
+        paddingBottom: 1,
+        paddingLeft: 1,
+        paddingRight: 2,
+        flexGrow: 1,
       }}
     >
-      <box
-        style={{
-          height: 1,
-          flexGrow: 1,
-          borderStyle: 'single',
-          border: ['top'],
-          borderColor: backgroundColor,
-          customBorderChars: capChars('▄'),
-        }}
-      />
+      {children}
     </box>
-    <box
-      style={{
-        borderStyle: 'single',
-        border: ['left'],
-        borderColor,
-        customBorderChars: { ...BOX_CHARS, vertical: '┃' },
-      }}
-    >
-      <box
-        style={{
-          flexDirection: 'row',
-          backgroundColor,
-          paddingLeft: 1,
-          paddingRight: 2,
-          flexGrow: 1,
-        }}
-      >
-        {children}
-      </box>
-    </box>
-    <box
-      style={{
-        height: 1,
-        borderStyle: 'single',
-        border: ['left'],
-        borderColor,
-        customBorderChars: edgeChars('╹'),
-      }}
-    >
-      <box
-        style={{
-          height: 1,
-          flexGrow: 1,
-          borderStyle: 'single',
-          border: ['bottom'],
-          borderColor: backgroundColor,
-          customBorderChars: capChars('▀'),
-        }}
-      />
-    </box>
-  </>
+  </box>
 )
