fd5c2c395b071b0a16bbb7be041eceafb7f41494
trg <tom@magnitude.dev>
2026-09-13 03:59:00 -0700

    Recover transient installer negotiation without accepting inconsistent bytes

--- packages/release/src/hosted-update/installer-download.ts
+++ packages/release/src/hosted-update/installer-download.ts
@@ -1,4 +1,4 @@
-import { Option } from "effect"
+import { Effect, Option } from "effect"
 import { defaultArtifactDownloadPolicy, downloadArtifact, type ArtifactDownloadInput } from "../artifact-download"
 import type { UpdateManifest } from "./manifest"
 
@@ -14,4 +14,4 @@ export const downloadUpdateArtifact = (options: {
   strategy: { _tag: "Segmented", concurrency: 4, chunkBytes: 4 * 1024 * 1024, fallbackToSequential: true },
   policy: { ...defaultArtifactDownloadPolicy, attemptTimeout: "1 hour", totalTimeout: "185 minutes" },
   onProgress: options.onProgress, onVerificationProgress: Option.none(),
-})
+}).pipe(Effect.tapError(error => Effect.logDebug("Installer transfer failed", { phase: error.phase, message: error.message })))
